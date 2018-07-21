c4912ec3fa2e685c9def1c46fcb750f3520468e7
Clint Byrum <clint@fewbar.com>
2018-07-20 17:14:25 -0700

    BROKEN: but fun

--- uptimerobot_test.go
+++ uptimerobot_test.go
@@ -1,17 +1,25 @@
 package uptimerobot
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
     "github.com/stretchr/testify/assert"
+    "encoding/json"
 )
 
 func _makeTestServer() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Uptimerobot")
+        e := json.NewEncoder(w)
+        cmon := CreateMonitorResp {
+            stat: "ok",
+            monitor: CreatedMonitor {
+                id: "0",
+                status: 0,
+            },
+        }
+        e.Encode(cmon)
 	}))
 }
 
@@ -30,5 +38,5 @@ func TestMakeMonitor(t *testing.T) {
 	}
     m, err := c.getMonitors()
     assert.Equal(t, nil, err)
-    assert.Equal(t, 0, len(m))
+    assert.Equal(t, "ok", m.stat)
 }
